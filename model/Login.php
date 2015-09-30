e1e5b80814462fbde6d65ae76eace64ad473c549
Lisa Westlund <lw222gu@student.lnu.se>
2015-09-30 22:37:32 +0200

    Checks user file and compares passwords

--- model/Login.php
+++ model/Login.php
@@ -5,8 +5,8 @@ namespace model;
 session_start();
 
 class Login {
-    private static $username = "Admin";
-    private static $password = "Password";
+   // private static $username = "Admin";
+   // private static $password = "Password";
     private $isLoggedInSession = "isLoggedIn";
 
     public function __construct() {
@@ -18,11 +18,32 @@ class Login {
 
     public function checkUserInput($username, $password){
 
+        if(file_exists("data/" . $username . ".txt")){
+            $userFile = fopen("data/" . $username . ".txt", "r");
+            if(password_verify($password, fgets($userFile))){
+                fclose($userFile);
+                $_SESSION[$this->isLoggedInSession] = true;
+                return true;
+            }
+
+            //CONTINUE HERE: Instead of throwing exception, return false, and
+            //set if false in controller to be able to create this error from view.
+            else {
+                fclose($userFile);
+                throw new \Exception("Wrong name or password");
+            }
+        }
+
+        //THIS NEEDS REFACTORING!!!!
+        else {
+            throw new \Exception("Wrong name or password");
+        }
+
         //NEEDS TO BE REWRITTEN!
         //User credentials are to be compared to the ones in the userdatabase file.
 
         // Validation in model, if validation in view for some reason wont work...
-        if(empty($username)){
+       /* if(empty($username)){
             throw new \Exception("Username is missing");
         }
 
@@ -33,10 +54,10 @@ class Login {
 
         if($username !== self::$username || $password !== self::$password){
             throw new \Exception("Wrong name or password");
-        }
+        }*/
 
-        $_SESSION[$this->isLoggedInSession] = true;
-        return true;
+        //$_SESSION[$this->isLoggedInSession] = true;
+        //return true;
     }
 
     public function checkIfLoggedIn(){
